c4961df91125a46311c558a223afb15c00bcd699
firoz.nur <firoznur5@gmail.com>
2024-04-29 12:33:02 +0600

    error page and nested route setup done

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+  return (
+    <div>
+<h1>Oops!</h1>
+<p>wrong route</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root";
+import ErrorPage from "./ErrorPage";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
