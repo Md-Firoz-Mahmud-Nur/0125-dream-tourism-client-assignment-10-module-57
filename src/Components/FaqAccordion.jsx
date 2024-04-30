75a7f48111f0d5b15fadeefdc83b94425d0e80cd
firoz.nur <firoznur5@gmail.com>
2024-04-30 08:43:35 +0600

    aos animation to faq

--- src/Components/FaqAccordion.jsx
+++ src/Components/FaqAccordion.jsx
@@ -12,8 +12,17 @@ const FaqAccordion = () => {
           information.
         </p>
       </h1>
-      <div className="my-10 flex flex-col gap-4 rounded-xl border border-black p-4">
-        <div className="collapse collapse-arrow bg-base-200 ">
+      <div
+        data-aos="fade-up"
+        data-aos-duration="2000"
+        className="my-10 flex flex-col gap-4 rounded-xl border border-black p-4"
+      >
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="50"
+          className="collapse collapse-arrow bg-base-200 "
+        >
           <input type="radio" name="my-accordion-2" defaultChecked />
           <div className="collapse-title text-xl font-medium">
             How can I book a tour?
@@ -22,7 +31,12 @@ const FaqAccordion = () => {
             <p>Explore our simple tour booking process.</p>
           </div>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="100"
+          className="collapse collapse-arrow bg-base-200"
+        >
           <input type="radio" name="my-accordion-2" />
           <div className="collapse-title text-xl font-medium">
             What destinations do you offer tours to?
@@ -31,7 +45,12 @@ const FaqAccordion = () => {
             <p>Discover our diverse range of travel destinations.</p>
           </div>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="150"
+          className="collapse collapse-arrow bg-base-200"
+        >
           <input type="radio" name="my-accordion-2" />
           <div className="collapse-title text-xl font-medium">
             Can I customize my tour itinerary?
@@ -40,7 +59,12 @@ const FaqAccordion = () => {
             <p>Learn about our flexible travel options.</p>
           </div>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="200"
+          className="collapse collapse-arrow bg-base-200"
+        >
           <input type="radio" name="my-accordion-2" />
           <div className="collapse-title text-xl font-medium">
             What safety measures are in place during tours?
@@ -49,7 +73,12 @@ const FaqAccordion = () => {
             <p>Find out about our commitment to traveler safety.</p>
           </div>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="250"
+          className="collapse collapse-arrow bg-base-200"
+        >
           <input type="radio" name="my-accordion-2" />
           <div className="collapse-title text-xl font-medium">
             How can I cancel or modify my booking?
@@ -60,7 +89,12 @@ const FaqAccordion = () => {
             </p>
           </div>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="300"
+          className="collapse collapse-arrow bg-base-200"
+        >
           <input type="radio" name="my-accordion-2" />
           <div className="collapse-title text-xl font-medium">
             What should I pack for my trip?
