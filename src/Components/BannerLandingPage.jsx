7fe0027cb066586809d0ebee48cd0559e4174c00
firoz.nur <firoznur5@gmail.com>
2024-04-30 06:42:36 +0600

    add aos in the banner

--- src/Components/BannerLandingPage.jsx
+++ src/Components/BannerLandingPage.jsx
@@ -1,8 +1,15 @@
 const BannerLandingPage = () => {
   return (
-    <h1 className="my-5 rounded-t-lg  py-10 text-center text-4xl font-semibold ">
+    <h1
+      data-aos="fade-right"
+      data-aos-duration="1500"
+      className="my-5 rounded-t-lg  py-10 text-center text-4xl font-semibold "
+    >
       Experience modern tourism and hassle-free service.
-      <p className="mt-2 text-lg">Explore your ideal destination effortlessly with our innovative and stress-free approach.</p>
+      <p className="mt-2 text-lg">
+        Explore your ideal destination effortlessly with our innovative and
+        stress-free approach.
+      </p>
     </h1>
   );
 };
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,7 @@
 import BannerLandingPage from "./BannerLandingPage";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Home = () => {
   return (
