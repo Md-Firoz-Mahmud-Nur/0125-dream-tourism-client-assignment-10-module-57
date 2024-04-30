5d1690ff9f446c5a6a8f95d8cd7b6603640106fa
firoz.nur <firoznur5@gmail.com>
2024-04-30 11:29:54 +0600

    carousel with approait text done

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -7,9 +7,16 @@ const Carousel = () => {
     >
       <div id="slide1" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/bLrZBHY/9-800x467.webp"
+          src="https://i.ibb.co/cbDtdCy/Screenshot-2024-04-30-110549.png"
           className="w-full object-cover"
         />
+        <div className="absolute bottom-5  left-10 right-10 rounded-xl border border-black bg-base-100 bg-opacity-60 p-4 text-center text-2xl font-bold">
+          <h1>Sundorban</h1>
+          <p className="text-xl font-medium">
+            Bangladesh&apos;s majestic mangrove forest, home to diverse wildlife
+            and stunning natural beauty.
+          </p>
+        </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a
             href="#slide4"
@@ -27,9 +34,16 @@ const Carousel = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/RSGX6nB/9-5-800x467.webp"
+          src="https://i.ibb.co/C9gLrPm/malayasia.png"
           className="w-full object-cover"
         />
+        <div className="absolute bottom-5  left-10 right-10 rounded-xl border border-black bg-base-100 bg-opacity-60 p-4 text-center text-2xl font-bold">
+          <h1>Langkawi</h1>
+          <p className="text-xl font-medium">
+            Malaysia&apos;s tropical paradise, offering pristine beaches, lush
+            rainforests, and vibrant culture.
+          </p>
+        </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a
             href="#slide1"
@@ -47,9 +61,17 @@ const Carousel = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/0j2WMW7/2-9-835x467.webp"
+          src="https://i.ibb.co/b7rNKnH/Hoi-An-Ancient-Town.png"
           className="w-full object-cover"
         />
+        <div className="absolute bottom-5  left-10 right-10 rounded-xl border border-black bg-base-100 bg-opacity-60 p-4 text-center text-2xl font-bold">
+          <h1>Hoi An Ancient Town</h1>
+          <p className="text-xl font-medium">
+            A charming UNESCO World Heritage site in Vietnam, known for its
+            well-preserved architecture, lantern-lit streets, and rich cultural
+            heritage.
+          </p>
+        </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a
             href="#slide2"
@@ -67,9 +89,17 @@ const Carousel = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/0m4Mm4x/1-XO-Hotel-Inner-Twin-room.jpg"
+          src="https://i.ibb.co/PQS04xq/Phnom-Penh.png"
           className="w-full object-cover"
         />
+        <div className="absolute bottom-5  left-10 right-10 rounded-xl border border-black bg-base-100 bg-opacity-60 p-4 text-center text-2xl font-bold">
+          <h1>Phnom Penh</h1>
+          <p className="text-xl font-medium">
+            The vibrant capital city of Cambodia, brimming with historic
+            landmarks, bustling markets, and a blend of Khmer and French
+            colonial architecture.
+          </p>
+        </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a
             href="#slide3"
